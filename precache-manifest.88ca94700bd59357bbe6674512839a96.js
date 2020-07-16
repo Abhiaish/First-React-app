2a6c243181adb74073b01e49de5e44d9ecd52cc1
abhinaya <abhinayaaish95@gmail.com>
2020-07-16 10:42:23 +0530

    Updates

---
+++ precache-manifest.88ca94700bd59357bbe6674512839a96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "433d62e54ead986973e1a6e117c5741c",
+    "url": "/First-React-app/index.html"
+  },
+  {
+    "revision": "1515fbe392c747ddb56d",
+    "url": "/First-React-app/static/css/main.cbe52dbf.chunk.css"
+  },
+  {
+    "revision": "36c62ae4149a7b0bd095",
+    "url": "/First-React-app/static/js/2.98cb6f04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/First-React-app/static/js/2.98cb6f04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1515fbe392c747ddb56d",
+    "url": "/First-React-app/static/js/main.7dfe433a.chunk.js"
+  },
+  {
+    "revision": "452711e1d2700ccd1be8",
+    "url": "/First-React-app/static/js/runtime-main.75afddef.js"
+  }
+]);
